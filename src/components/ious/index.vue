acf9bf2eaa2a83a48553c8b88a6e40537495b33e
shaw-555 <shaw18664590570@163.com>
2021-05-24 21:02:10 +0800

    开始ious页面

--- src/components/ious/index.vue
+++ src/components/ious/index.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>
-    ious
+  <div class="wrapper">
+    <heador/>
+    <navbar />
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Ious'
+//import panel from '../../core/panel.vue'
+import heador from '../../public/heador.vue'
+import navbar from '../../public/navbar.vue'
+
+export default{
+  name: 'Thehome',
+  components: {
+    //panel,
+    navbar,
+    heador
+  }
+
 }
 </script>
+
+<style lang="scss" module>
+  @import '../../assets/styles/reset.scss';
+</style>
